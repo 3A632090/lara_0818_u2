e216a2f787ebdff6f109894f960fbb7059f0163f
M511-18\admin <s3a632090@student.ncut.edu.tw>
2019-10-30 11:08:05 +0800

    從 Controller 將參數傳遞至 view

--- lara_0818_u2/routes/web.php
+++ lara_0818_u2/routes/web.php
@@ -14,6 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home',function(){
-    return view('home.index');
-});
+//Route::get('home',function(){
+   // return view('home.index');
+//});
+Route::get('hello/{name?}',['as'=>'hello.index','uses'=>
+    'HelloController@index']);
